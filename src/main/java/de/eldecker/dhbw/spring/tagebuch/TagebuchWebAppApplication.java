56e798434798858670ed99155d9012b04fa6b298
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-18 16:05:28 +0200

    Projekt von start.spring.io

---
+++ src/main/java/de/eldecker/dhbw/spring/tagebuch/TagebuchWebAppApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.tagebuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagebuchWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagebuchWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/tagebuch/TagebuchWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.tagebuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagebuchWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
