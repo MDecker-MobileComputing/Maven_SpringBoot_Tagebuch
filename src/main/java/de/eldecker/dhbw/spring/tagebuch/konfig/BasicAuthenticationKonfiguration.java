00562a0ad8545f219f734e35df1cfd66ac439074
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-18 19:08:18 +0200

    Update BasicAuthenticationKonfiguration.java

--- src/main/java/de/eldecker/dhbw/spring/tagebuch/konfig/BasicAuthenticationKonfiguration.java
+++ src/main/java/de/eldecker/dhbw/spring/tagebuch/konfig/BasicAuthenticationKonfiguration.java
@@ -1,5 +1,8 @@
 package de.eldecker.dhbw.spring.tagebuch.konfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,30 +20,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class BasicAuthenticationKonfiguration {
 
+    private Logger LOG = LoggerFactory.getLogger( BasicAuthenticationKonfiguration.class );
+
+    private static final String ROLLE_NUTZER = "nutzer";
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf( (csrf) -> csrf.disable() )
-                    .authorizeHttpRequests()
-                    .anyRequest()
-                    //.requestMatchers("/app")
-                    .authenticated()                    
-                    .and()
+        httpSecurity.csrf( (csrf) -> csrf.disable() ) // https://stackoverflow.com/a/74753955
+                    .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/app")
+                                .hasRole(ROLLE_NUTZER)
+                                .anyRequest().authenticated() )
                     .httpBasic();
 
         return httpSecurity.build();
     }
-    
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        
+
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("alice")
                 .password("g3h3im")
-                .roles("NUTZER")
+                .roles(ROLLE_NUTZER)
                 .build();
-        
+
         return new InMemoryUserDetailsManager(user);
-    }    
+    }
 
 }
