af1dd2330181185c69542d2a0eb5f8b457f0d58e
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-20 17:39:48 +0200

    Klasse BasicAuthenticationKonfiguration verbesser (u.a. Kommentare)

--- src/main/java/de/eldecker/dhbw/spring/tagebuch/konfig/BasicAuthenticationKonfiguration.java
+++ src/main/java/de/eldecker/dhbw/spring/tagebuch/konfig/BasicAuthenticationKonfiguration.java
@@ -1,18 +1,17 @@
 package de.eldecker.dhbw.spring.tagebuch.konfig;
 
+import static org.springframework.security.crypto.factory.PasswordEncoderFactories.createDelegatingPasswordEncoder;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -37,6 +36,13 @@ public class BasicAuthenticationKonfiguration {
                    .build();
     }
 
+    /**
+     * Nutzernamen mit Passwörtern definieren.
+     * 
+     * @param passwordEncoder Bean für Kodierung Passwörter.
+     * 
+     * @return
+     */
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
 
@@ -53,10 +59,16 @@ public class BasicAuthenticationKonfiguration {
         return new InMemoryUserDetailsManager(user1, user2);
     }
     
+    
+    /**
+     * Bean für Passwort-Kodierung erzeugen.
+     * 
+     * @return Bean für Passwort-Kodierung
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        PasswordEncoder encoder = createDelegatingPasswordEncoder();
         return encoder;
     }    
 
