b4a2534572a6d4cf32eacc8b43097c4d1abfbb6a
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-22 10:01:43 +0200

    package-info fuer Base-Package dazu

---
+++ src/main/java/de/eldecker/dhbw/spring/tagebuch/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Einfache Tagebuch-Webapp mit <i>Spring Boot</i>, zur Demonstration von
+ * Basic Authentication und Persistenz mit Klasse {@code JdbcTemplate}.
+ */
+package de.eldecker.dhbw.spring.tagebuch;
