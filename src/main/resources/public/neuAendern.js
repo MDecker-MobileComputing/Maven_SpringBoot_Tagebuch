505054d5c28a92c09da2ed9a2533e5a67662fe60
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-25 18:06:30 +0200

    neuAendern.js refactored

--- src/main/resources/public/neuAendern.js
+++ src/main/resources/public/neuAendern.js
@@ -1,14 +1,32 @@
 "use strict";
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+/**
+ * Hilfsunktion: Wert von bestimmten Cookie auslesen.
+ *
+ * @param {*} name Name des auszulesenden Cookies, v.a. "JSESSIONID"
+ *
+ * @returns Wert des Cookies oder leerer String, falls nicht gefunden
+ */
+function holeCookie(name) {
+
+    const nameEQ      = name + "=";
+    const cookieArray = document.cookie.split( ";" );
+
+    for(let i = 0; i < cookieArray.length; i++) {
+
+        let cookie = cookieArray[i];
+        while ( cookie.charAt(0) === ' ' ) {
+
+            cookie = cookie.substring( 1, cookie.length );
+        }
+        if ( cookie.indexOf( nameEQ ) === 0) {
+
+            return cookie.substring( nameEQ.length, cookie.length );
+        }
     }
-    return null;
+
+    console.error( `Cookie "${name}" nicht gefunden.` );
+    return "";
 }
 
 /**
@@ -34,7 +52,7 @@ function onSpeichernButton() {
 
     beitrag = beitrag.trim();
 
-    const jsessionId = getCookie("JSESSIONID"); // Session-ID aus Cookie auslesen, um sie beim POST-Request mitzugeben
+    const jsessionId = holeCookie( "JSESSIONID" ); // Session-ID aus Cookie auslesen, um sie beim POST-Request mitzugeben
 
     fetch( "/api/v1/eintrag", {
         method: "POST",
@@ -48,7 +66,7 @@ function onSpeichernButton() {
         if (!response.ok) {
 
             const statusText = `${response.statusText} (${response.status})`;
-            throw new Error(`REST-Endpunkt hat Fehlercode zurückgeliefert: ${statusText}`);
+            throw new Error( `REST-Endpunkt hat Fehlercode zurückgeliefert: ${statusText}` );
 
         } else {
 
