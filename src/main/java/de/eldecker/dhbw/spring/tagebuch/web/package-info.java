6418569143883e26e7a1f56ddcb710adf83875a0
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-04-26 16:59:02 +0200

    PreparedStatements.properties in Unterordner sql/ verschoben

--- src/main/java/de/eldecker/dhbw/spring/tagebuch/web/package-info.java
+++ src/main/java/de/eldecker/dhbw/spring/tagebuch/web/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Dieses Paket enthält Controller für die Thymeleaf-Views und RestController 
  * für die REST-Endpunkte. Die Authentifizierung (Basic Authentication) wird 
- * in der Klasse {@link BasicAuthenticationKonfiguration} konfiguriert. 
+ * in der Klasse {@code BasicAuthenticationKonfiguration} konfiguriert. 
  */
 package de.eldecker.dhbw.spring.tagebuch.web;
